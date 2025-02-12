ce0bad5e504bdde0324d1f61e0d61f515a125322
RekZNoZ <rafikicroz@hotmail.com>
2025-02-12 23:22:01 +0100

    Create model.php

---
+++ web/db/model.php
@@ -0,0 +1,63 @@
+<?php
+require_once '../db/pdo.php';
+
+class Model
+{
+    private static $instance = null;
+    private $conn;
+
+    // Constructor para establecer la conexión
+    private function __construct()
+    {
+        global $conn;  // Asumimos que $conn es una conexión PDO ya establecida
+        $this->conn = $conn;
+    }
+
+    // Método para obtener la instancia del modelo
+    public static function getInstance(): ?Model
+    {
+        if (self::$instance === null) {
+            self::$instance = new Model();
+        }
+
+        return self::$instance;
+    }
+
+    // Crear Usuario usando password_hash
+    public function crearUsuario($usuario, $contrasena): false|string
+    {
+        $sql = "INSERT INTO usuarios (usuario, contrasena) VALUES (:usuario, :contrasena)";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bindParam(':usuario', $usuario);
+        $contrasenaEncriptada = password_hash($contrasena, PASSWORD_DEFAULT);
+        $stmt->bindParam(':contrasena', $contrasenaEncriptada);
+        $stmt->execute();
+        return $this->conn->lastInsertId();
+    }
+
+    // Verificar si el usuario existe
+    public function verificarUsuario($usuario)
+    {
+        $sql = "SELECT * FROM usuarios WHERE usuario = :usuario";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bindParam(':usuario', $usuario);
+        $stmt->execute();
+        return $stmt->fetch();
+    }
+
+    // Obtener Usuario para login y verificar la contraseña con password_verify
+    public function obtenerUsuario($usuario, $contrasena)
+    {
+        $sql = "SELECT * FROM usuarios WHERE usuario = :usuario";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bindParam(':usuario', $usuario);
+        $stmt->execute();
+        $usuario = $stmt->fetch();
+
+        if ($usuario && password_verify($contrasena, $usuario['contrasena'])) {
+            return $usuario;
+        }
+
+        return false; // Usuario no encontrado o contraseña incorrecta
+    }
+}
