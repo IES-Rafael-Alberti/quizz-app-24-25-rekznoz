fc372246b442b82ea070d003f6e2604cf84b6614
RekZNoZ <rafikicroz@hotmail.com>
2025-02-12 20:29:52 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,1 +1,9 @@
-<?php
+<!doctype html>
+<html lang="es">
+<?php include 'plantillas/head.php'; ?>
+<body class="contenedor-principal">
+<?php include 'plantillas/header.php'; ?>
+<?php include 'plantillas/main.php'; ?>
+<?php include 'plantillas/footer.php'; ?>
+</body>
+</html>
