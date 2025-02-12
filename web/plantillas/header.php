be0073350ccef9bb8bd966061fb09bdcd8a6d0a2
RekZNoZ <rafikicroz@hotmail.com>
2025-02-12 20:30:00 +0100

    Create header.php

---
+++ web/plantillas/header.php
@@ -0,0 +1,11 @@
+<header class="header">
+    <h1>Aplicación web</h1>
+    <nav>
+        <ul>
+            <li><a href="index.php">Inicio</a></li>
+            <li><a href="pagina1.php">Página 1</a></li>
+            <li><a href="pagina2.php">Página 2</a></li>
+            <li><a href="pagina3.php">Página 3</a></li>
+        </ul>
+    </nav>
+</header>
