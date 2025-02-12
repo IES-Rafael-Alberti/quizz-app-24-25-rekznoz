e1b050e1a3432b1a60c42b2d0374bf7dc7efe17f
RekZNoZ <rafikicroz@hotmail.com>
2025-02-12 22:11:37 +0100

    Update main.php

--- web/plantillas/main.php
+++ web/plantillas/main.php
@@ -1,3 +1,7 @@
 <main class="main">
-    <p>asdasdasdasdasasasd</p>
+    <div class="animacion-inicio">
+        <h1 class="titulo-inicio">¡Bienvenido a Quizz APP!</h1>
+        <p class="descripcion-inicio">¡Aquí podrás encontrar los mejores quizz para ti!</p>
+        <a href="quizz.php" class="boton-inicio">¡Comenzar!</a>
+    </div>
 </main>
