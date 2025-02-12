7f41c96549959b4ae2de35d6d42209da4984ae7f
RekZNoZ <rafikicroz@hotmail.com>
2025-02-12 23:51:24 +0100

    Update header.php

--- web/plantillas/header.php
+++ web/plantillas/header.php
@@ -4,12 +4,26 @@
             <li class="nav-item">
                 <a href="index.php" class="nav-link">Inicio</a>
             </li>
-            <li class="nav-item">
-                <a href="login.php" class="nav-link">Login</a>
-            </li>
-            <li class="nav-item">
-                <a href="registro.php" class="nav-link">Registro</a>
-            </li>
+            <?php
+            // Comprobar si el usuario está logueado
+            if (isset($_SESSION['usuario'])) {
+                // Mostrar opciones para usuarios logueados
+                echo '<li class="nav-item">
+                        <a href="usuario/usuario-logout.php" class="nav-link">Cerrar sesión</a>
+                      </li>
+                      <li class="nav-item">
+                        <a href="usuario/usuario-perfil.php" class="nav-link">Perfil</a>
+                      </li>';
+            } else {
+                // Mostrar opciones para usuarios no logueados
+                echo '<li class="nav-item">
+                        <a href="login.php" class="nav-link">Iniciar sesión</a>
+                      </li>
+                      <li class="nav-item">
+                        <a href="registro.php" class="nav-link">Registrarse</a>
+                      </li>';
+            }
+            ?>
         </ul>
     </nav>
 </header>
