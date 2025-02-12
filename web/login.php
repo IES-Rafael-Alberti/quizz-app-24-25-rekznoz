4d2771ca209467dcc275064dee37909fdd8f57ae
RekZNoZ <rafikicroz@hotmail.com>
2025-02-12 22:40:45 +0100

    Create login.php

---
+++ web/login.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="es">
+<?php include 'plantillas/head.php'; ?>
+<body class="contenedor-principal">
+<?php include 'plantillas/header.php'; ?>
+<main class="main">
+    <div class="contenedor-formulario-login">
+        <h1 class="titulo-formulario-login">Iniciar sesión</h1>
+        <form action="login.php" method="post" class="formulario-login">
+            <div class="input-container">
+                <input type="text" name="usuario" id="usuario" class="input-formulario-login" required placeholder=" ">
+                <label for="usuario" class="label-formulario-login">Usuario</label>
+            </div>
+            <div class="input-container">
+                <input type="password" name="contrasena" id="contrasena" class="input-formulario-login" required placeholder=" ">
+                <label for="contrasena" class="label-formulario-login">Contraseña</label>
+            </div>
+            <input type="submit" value="Iniciar sesión" class="boton-formulario-login">
+        </form>
+        <?php
+            if (isset($_GET["error-login"])) {
+                echo '<span class="error-login">' . $_GET["error-login"] . "</span>";
+            }
+        ?>
+    </div>
+</main>
+<?php include 'plantillas/footer.php'; ?>
+</body>
+</html>
