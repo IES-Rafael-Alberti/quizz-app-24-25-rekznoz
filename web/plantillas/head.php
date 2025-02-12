2776acb9263529fabf3dbb4f874f54602b45064e
RekZNoZ <rafikicroz@hotmail.com>
2025-02-12 20:29:57 +0100

    Create head.php

---
+++ web/plantillas/head.php
@@ -0,0 +1,11 @@
+<title>Quizz APP</title>
+<link rel="icon" type="image/png" href="img/logo.png" />
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta property="og:title" content="Quizz APP">
+<meta property="og:description" content="¡Esta es la página web de Quizz APP!">
+<meta property="og:type" content="website">
+<meta name="description" content="¡Esta es la página web de Quizz APP!" />
+<meta name="keywords" content="quizz, app, web, página, inicio" />
+<meta charset="UTF-8">
+<link rel="stylesheet" href="css/import.css">
