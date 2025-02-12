e46f0c5112547fe44764d90764e55aeb50e77850
RekZNoZ <rafikicroz@hotmail.com>
2025-02-12 22:44:51 +0100

    Update registro.php

--- web/registro.php
+++ web/registro.php
@@ -1,2 +1,33 @@
+<!doctype html>
+<html lang="es">
+<?php include 'plantillas/head.php'; ?>
+<body class="contenedor-principal">
+<?php include 'plantillas/header.php'; ?>
 <main class="main">
+    <div class="contenedor-formulario-registro">
+        <h1 class="titulo-formulario-registro">Iniciar sesión</h1>
+        <form action="registro.php" method="post" class="formulario-registro">
+            <div class="input-container">
+                <input type="text" name="usuario" id="usuario" class="input-formulario-registro" required placeholder=" ">
+                <label for="usuario" class="label-formulario-registro">Usuario</label>
+            </div>
+            <div class="input-container">
+                <input type="password" name="contrasena" id="contrasena" class="input-formulario-registro" required placeholder=" ">
+                <label for="contrasena" class="label-formulario-registro">Contraseña</label>
+            </div>
+            <div class="input-container">
+                <input type="password" name="contrasena2" id="contrasena2" class="input-formulario-registro" required placeholder=" ">
+                <label for="contrasena2" class="label-formulario-registro">Repetir contraseña</label>
+            </div>
+            <input type="submit" value="Iniciar sesión" class="boton-formulario-registro">
+        </form>
+        <?php
+        if (isset($_GET["error-registro"])) {
+            echo '<span class="error-registro">' . $_GET["error-registro"] . "</span>";
+        }
+        ?>
+    </div>
 </main>
+<?php include 'plantillas/footer.php'; ?>
+</body>
+</html>
