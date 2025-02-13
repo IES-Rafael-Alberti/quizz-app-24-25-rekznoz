f61d5fd4ee7658725f74ab12e40f928248f86abb
RekZNoZ <rafikicroz@hotmail.com>
2025-02-13 20:15:00 +0100

    Update quizz-editar.php

--- web/quizz/quizz-editar.php
+++ web/quizz/quizz-editar.php
@@ -1,1 +1,25 @@
 <?php
+require_once '../db/Quizz.php';
+session_start();
+if (!isset($_SESSION['usuario'])) {
+    header('Location: ../vista/login.php');
+    exit;
+}
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $quizId = $_POST['quiz_id'];
+
+    $quizz = Quizz::getInstance();
+
+    $quiz = $quizz->obtenerQuizPorId($quizId);
+
+    $titulo = $_POST['titulo'];
+    $descripcion = $_POST['descripcion'];
+
+    $quizz->actualizarQuiz($quizId, $titulo, $descripcion);
+    header('Location: ../vista/editar-preguntas.php?id=' . $quizId);
+    exit;
+
+}
+
