b14746e6f0ee917b250e3797ca01f30171305e39
RekZNoZ <rafikicroz@hotmail.com>
2025-02-13 16:14:26 +0100

    Create index.php

---
+++ web/vista/index.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+?>
+<!doctype html>
+<html lang="es">
+<?php include '../plantillas/head.php'; ?>
+<body class="contenedor-principal">
+<?php include '../plantillas/header.php'; ?>
+<?php include '../plantillas/main.php'; ?>
+<?php include '../plantillas/footer.php'; ?>
+</body>
+</html>
