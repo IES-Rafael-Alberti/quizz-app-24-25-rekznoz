0842499ae56b882482febfc932452259d467569b
RekZNoZ <rafikicroz@hotmail.com>
2025-02-13 01:03:46 +0100

    Update pregunta.php

--- web/db/pregunta.php
+++ web/db/pregunta.php
@@ -1,5 +1,5 @@
 <?php
-require_once '../db/pdo.php'; // Asegúrate de que la conexión a la base de datos esté disponible
+require_once './db/pdo.php'; // Asegúrate de que la conexión a la base de datos esté disponible
 
 class Pregunta
 {
