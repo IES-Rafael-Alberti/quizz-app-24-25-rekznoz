e280f598f23c84d90fff4c04883b8020e006fbd6
RekZNoZ <rafikicroz@hotmail.com>
2025-02-13 16:56:01 +0100

    Update ver-quizz.php

--- web/vista/ver-quizz.php
+++ web/vista/ver-quizz.php
@@ -1,12 +1,17 @@
 <?php
 require_once '../db/Quizz.php';
+require_once '../db/Pregunta.php';
 session_start();
 
 if (isset($_GET['id'])) {
     $quizId = $_GET['id'];
+
     $quizz = Quizz::getInstance();
-    $quiz = $quizz->obtenerQuizz($quizId);
-    //$preguntas = $quizz->obtenerPreguntas($quizId);
+    $preguntas = Pregunta::getInstance();
+
+    $quiz = $quizz->obtenerQuizPorId($quizId);
+    $preguntas = $preguntas->obtenerPreguntasPorQuiz($quizId);
+
 } else {
     die("El cuestionario no existe.");
 }
@@ -17,20 +22,37 @@ if (isset($_GET['id'])) {
 <body class="contenedor-principal">
 <?php include '../plantillas/header.php'; ?>
 <main class="main">
-    <h1><?php echo $quiz['titulo']; ?></h1>
-    <p><?php echo $quiz['descripcion']; ?></p>
-    <form action="resultado.php" method="post">
-        <?php
-        // foreach ($preguntas as $pregunta) {
-        //     echo "<h2>{$pregunta['question']}</h2>";
-        //     echo "<input type='radio' name='pregunta_{$pregunta['question_id']}' value='1'>{$pregunta['option1']}<br>";
-        //     echo "<input type='radio' name='pregunta_{$pregunta['question_id']}' value='2'>{$pregunta['option2']}<br>";
-        //     echo "<input type='radio' name='pregunta_{$pregunta['question_id']}' value='3'>{$pregunta['option3']}<br>";
-        //     echo "<input type='radio' name='pregunta_{$pregunta['question_id']}' value='4'>{$pregunta['option4']}<br>";
-        // }
-        ?>
-        <button type="submit">Enviar</button>
-    </form>
+    <div class="contenedor-cuestionario-quizz">
+        <h1 class="titulo-cuestionario-quizz"><?php echo $quiz['titulo']; ?></h1>
+        <p class="descripcion-cuestionario-quizz"><?php echo $quiz['descripcion']; ?></p>
+        <form action="calificar-quizz.php" method="post">
+            <input type="hidden" name="quiz_id" value="<?php echo $quizId; ?>">
+            <?php foreach ($preguntas as $pregunta): ?>
+                <div class="pregunta">
+                    <p class="pregunta-texto"><?php echo $pregunta['pregunta_texto']; ?></p>
+                    <div class="opciones">
+                        <label>
+                            <input type="radio" name="pregunta_<?php echo $pregunta['pregunta_id']; ?>" value="a">
+                            <?php echo $pregunta['opcion_a']; ?>
+                        </label>
+                        <label>
+                            <input type="radio" name="pregunta_<?php echo $pregunta['pregunta_id']; ?>" value="b">
+                            <?php echo $pregunta['opcion_b']; ?>
+                        </label>
+                        <label>
+                            <input type="radio" name="pregunta_<?php echo $pregunta['pregunta_id']; ?>" value="c">
+                            <?php echo $pregunta['opcion_c']; ?>
+                        </label>
+                        <label>
+                            <input type="radio" name="pregunta_<?php echo $pregunta['pregunta_id']; ?>" value="d">
+                            <?php echo $pregunta['opcion_d']; ?>
+                        </label>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+            <button type="submit" class="boton-cuestionario-quizz">Calificar cuestionario</button>
+        </form>
+    </div>
 </main>
 <?php include '../plantillas/footer.php'; ?>
 </body>
