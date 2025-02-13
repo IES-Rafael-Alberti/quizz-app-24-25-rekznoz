05d5a55e80bc0d36a48cbd60d0d56da9e2354e4e
RekZNoZ <rafikicroz@hotmail.com>
2025-02-13 15:23:18 +0100

    Update perfil.php

--- web/perfil.php
+++ web/perfil.php
@@ -4,6 +4,9 @@ if (!isset($_SESSION['usuario'])) {
     header('Location: ../login.php');
     exit;
 }
+
+require_once './db/Quizz.php';
+
 ?>
 
 <!doctype html>
@@ -12,9 +15,31 @@ if (!isset($_SESSION['usuario'])) {
 <body class="contenedor-principal">
 <?php include 'plantillas/header.php'; ?>
 <main class="main">
-    <p>Bienvenido, <?php echo $_SESSION['usuario']; ?></p>
-    <a href="agregar-quizz.php">Agregar cuestionario</a>
-    <a href="mis-quizz.php">Mis cuestionarios</a>
+    <div class="inicio-perfil">
+        <p>Bienvenido, <?php echo $_SESSION['usuario']; ?></p>
+        <a href="agregar-quizz.php">Agregar cuestionario</a>
+    </div>
+    <div class="contenedor-cuestionarios">
+        <h1 class="titulo-mis-cuestionarios">Mis cuestionarios</h1>
+        <?php
+        $quizz = Quizz::getInstance();
+        $usuario_id = $_SESSION['id'];
+        $cuestionarios = $quizz->obtenerQuizzesPorUsuario($usuario_id);
+
+        if (empty($cuestionarios)) {
+            echo '<p class="sin-cuestionarios">No tienes cuestionarios.</p>';
+        } else {
+            foreach ($cuestionarios as $cuestionario) {
+                echo '<div class="cuestionario">';
+                echo '<h2 class="titulo-cuestionario">' . $cuestionario['titulo'] . '</h2>';
+                echo '<p class="descripcion-cuestionario">' . $cuestionario['descripcion'] . '</p>';
+                echo '<a href="editar-quizz.php?id=' . $cuestionario['quiz_id'] . '" class="boton-cuestionario">Editar cuestionario</a>';
+                echo '<a href="cuestionario.php?id=' . $cuestionario['quiz_id'] . '" class="boton-cuestionario">Ver cuestionario</a>';
+                echo '</div>';
+            }
+        }
+        ?>
+    </div>
 </main>
 <?php include 'plantillas/footer.php'; ?>
 </body>
