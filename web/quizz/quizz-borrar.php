5068edd32cb1ecf3f5f635294fd81c170e3da061
RekZNoZ <rafikicroz@hotmail.com>
2025-02-14 22:50:20 +0100

    Update quizz-borrar.php

--- web/quizz/quizz-borrar.php
+++ web/quizz/quizz-borrar.php
@@ -1,1 +1,15 @@
 <?php
+session_start();
+if (!isset($_SESSION['usuario'])) {
+    header('Location: login.php');
+    exit;
+}
+
+require_once '../db/Quizz.php';
+
+if ($_GET['id']) {
+    $quizz = Quizz::getInstance();
+    $quizz->eliminarQuiz($_GET['id']);
+
+    header('Location: ../vista/perfil.php');
+}
